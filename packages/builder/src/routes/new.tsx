d574b3d4058e51e804b33a27a9491b0cda8c739e
Fabian Sorqvist <fabian@sorqvist.nu>
2023-05-01 19:38:25 +0200

    more styling and ts fixes

--- packages/builder/src/routes/new.tsx
+++ packages/builder/src/routes/new.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Link, useOutletContext } from "react-router-dom";
-import { FormBuilderContext } from "../components/FormBuilder";
+import { FormBuilderContext } from "../router";
+import styles from "../styles";
 
 export default function NewComponent() {
   const context = useOutletContext<FormBuilderContext>();
   return (
     <section>
-      <h2>Add new component</h2>
-      {context.plugins.types.map((type, i) => (
-        <Link to={type.type.name} key={i}>
-          {type.type.title}
-        </Link>
-      ))}
+      <h2 className={styles.h2}>Add new component</h2>
+      <div className="flex flex-wrap">
+        {context.plugins.types.map((type, i) => (
+          <Link
+            className="bg-slate-300 p-3 mr-2 rounded hover:bg-slate-500"
+            to={type.type.name}
+            key={i}
+          >
+            {type.type.title}
+          </Link>
+        ))}
+      </div>
     </section>
   );
 }
