4328db05e75a3ed1a17076fd2bc267fd5805d212
Fabian Sorqvist <fabian@sorqvist.nu>
2023-07-20 19:45:36 +0200

    fix overlay

--- packages/builder/src/components/Overlay.tsx
+++ packages/builder/src/components/Overlay.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { Link, Outlet, useOutletContext } from "react-router-dom";
+import { Link, Outlet, useNavigate, useOutletContext } from "react-router-dom";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export default function Overlay() {
   const context = useOutletContext();
+  const navigate = useNavigate();
   return (
-    <div className="fixed h-screen w-screen top-0 left-0 bg-slate-100/75 bg-blend-lighten dark:bg-slate-800/75">
-      <div className="container max-w-3xl max-h-screen md:h-auto h-screen drop-shadow-xl border overflow-auto md:mt-10 mx-auto bg-white  p-4 rounded dark:bg-slate-800 dark:border-slate-600">
-        <Link className="dark:text-white" to="..">
-          Back
-        </Link>
-        <Outlet context={context} />
-      </div>
-    </div>
+    <Dialog.Root
+      open={true}
+      onOpenChange={() => {
+        navigate("..");
+      }}
+    >
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed h-screen w-screen top-0 left-0 bg-slate-100/75 bg-blend-lighten dark:bg-slate-800/75" />
+        <Dialog.Content className="container fixed top-20 left-1/4 max-w-screen-md max-h-screen md:h-auto h-screen drop-shadow-xl border overflow-auto  mx-auto bg-white p-4 rounded dark:bg-slate-800 dark:border-slate-600">
+          <Link className="dark:text-white" to="..">
+            Back
+          </Link>
+          <Outlet context={context} />
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
